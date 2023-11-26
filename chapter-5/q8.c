0b4d5fbd4d1e4f7c1c7a28d53d9a4d7b6dcc8463
samir01 <samir.os.abdalla@gmail.com>
2023-11-25 18:03:37 -0800

    Added answer for question 8

--- chapter-5/q8.c
+++ chapter-5/q8.c
@@ -6,30 +6,64 @@
 Write a program that creates two children, and connects the standard output of one
 to the standard input of the other, using the pipe() system call
 */
-#define MESSAGE_SIZE 14
-char *message = "child1 message";
+#define BUFFER_SIZE 20
 int main(int argc, char *argv[])
 {
-
-    int fds[] = {STDIN_FILENO, STDOUT_FILENO};
-    if (pipe(fds) < 0)
+    int p[2];
+    if ((pipe(p) < 0))
     {
-        printf("pipe error");
+        printf("p error");
     }
 
-    int child1 = fork();
+    int pid1, pid2;
+    pid1 = fork();
 
-    if (child1 < 0)
+    if (pid1 < 0)
     {
-        fprintf(stderr, "fork failed");
+        fprintf(stderr, " pid1 fork failed");
         exit(1);
     }
-    else if (child1 == 0)
+    else if (pid1 == 0)
     {
-        write(fds[1], message, MESSAGE_SIZE);
+        close(p[0]);
+        if (dup2(p[1], STDOUT_FILENO) < 0)
+        {
+            printf("error duplicating p1");
+            exit(1);
+        }
+
+        printf("message from child1");
     }
     else
     {
+        int status;
+        if ((pid2 = fork()) < 0)
+        {
+            fprintf(stderr, "pid2 fork failed");
+            exit(1);
+        }
+
+        if (pid2 == 0)
+        {
+            close(p[1]);
+            if (dup2(p[0], STDIN_FILENO))
+            {
+                printf("error duplicating p0");
+                exit(1);
+            }
+
+            char buf[BUFFER_SIZE];
+            read(STDIN_FILENO, buf, BUFFER_SIZE);
+            printf("Received message:%s\n", buf);
+        }
+        else
+        {
+            waitpid(pid1, NULL, 0);
+            waitpid(pid2, &status, WUNTRACED);
+        }
     }
     return 0;
 }
+
+/*Tough problem. I needed help from external resources GREATLY from this one but I ended
+up learning much more about pipes and dup2. WORTH*/
